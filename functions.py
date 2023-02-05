b087e71c81344d83095f7c2b9689747d1d3d12a3
Cal122 <callumwork1122@gmail.com>
2023-02-05 00:40:06 +0000

    Removed unneccesary self parameters

--- functions.py
+++ functions.py
@@ -1,13 +1,14 @@
 class Caluclator_Functions:
 
-    def add(self, input, input2):
+
+    def add(input, input2):
         return input + input2
     
-    def subtract(self, input, input2):
+    def subtract(input, input2):
         return input - input2
     
-    def multiply(self, input, input2):
+    def multiply(input, input2):
         return input * input2
     
-    def divide(self, input, input2):
+    def divide(input, input2):
         return input / input2
